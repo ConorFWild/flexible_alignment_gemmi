924b87bd6440af850a34868ade6211862481cb3d
cwild <conor.wild@sky.com>
2021-12-21 10:34:35 +0000

    Fix readme

---
+++ test/test.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import fire
+
+from flexible_alignment import Dataset, Reference, Alignment, Grid, Xmap, StructureFactors, Dtag
+
+
+def test(
+        reference_dtag,
+        reference_pdb_path,
+        reference_mtz_path,
+        moving_dtag,
+        moving_pdb_path,
+        moving_mtz_path,
+        output_map_path,
+        outer_mask_radius=6.0,
+        inner_mask_symmetry=2.0,
+        sample_rate=3.0,
+        f="FWT",
+        phi="PHWT",
+):
+    reference_pdb_path = Path(reference_pdb_path)
+    reference_mtz_path = Path(reference_mtz_path)
+    moving_pdb_path = Path(moving_pdb_path)
+    moving_mtz_path = Path(moving_mtz_path)
+    output_map_path = Path(output_map_path)
+
+    structure_factors = StructureFactors(f, phi)
+
+    datasets = {
+        Dtag(reference_dtag): Dataset.from_files(
+            reference_pdb_path,
+            reference_mtz_path,
+        ),
+        Dtag(moving_dtag): Dataset.from_files(
+            moving_pdb_path,
+            moving_mtz_path,
+        ),
+    }
+
+    reference = Reference.from_datasets(datasets)
+
+    alignments = {}
+    for dtag, dataset in datasets.items():
+        alignments[dtag] = Alignment.from_dataset(reference, datasets[dtag])
+
+    grid = Grid.from_reference(
+        reference,
+        outer_mask_radius,
+        inner_mask_symmetry,
+        sample_rate=sample_rate,
+    )
+
+    flexibly_aligned_xmaps = {}
+    for dtag, dataset in datasets.items():
+        flexibly_aligned_xmaps[dtag] = Xmap.from_unaligned_dataset(
+            dataset,
+            alignments[dtag],
+            grid=grid,
+            structure_factors=structure_factors,
+        )
+
+    flexibly_aligned_xmaps[Dtag(moving_dtag)].save(output_map_path)
+
+
+if __name__ == "__main__":
+    fire.Fire(test)
